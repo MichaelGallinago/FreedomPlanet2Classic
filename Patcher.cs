f172bb6ad802036e1c8a94c1a28d6207905d1f7e
Michael Gallinago <michael.gallinago@gmail.com>
2023-01-20 00:49:31 +0500

    Done

---
+++ Patcher.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace FP2Rebalance
+{
+    public class Patcher
+    {
+        public static FieldInfo GetPlayerField(string name) => typeof(FPPlayer).GetField(name, BindingFlags.Instance | BindingFlags.NonPublic);
+
+        public static void SetPlayerValue(string name, object value, object player = null) => GetPlayerField(name).SetValue(player, value);
+
+        public static object GetPlayerValue(string name, object player = null) => GetPlayerField(name).GetValue(player);
+
+        public static FPPlayer GetPlayer => FPStage.currentStage.GetPlayerInstance_FPPlayer();
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace FP2Rebalance
+{
+    [BepInPlugin("com.micg.plugins.fp2.classic", "FP2Classic", "1.0.0")]
+    [BepInProcess("FP2.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Harmony HarmonyLink { get; } = new Harmony("com.micg.plugins.fp2.classic");
+
+        private void Awake()
+        {
+            HarmonyLink.PatchAll(typeof(Start));
+        }
+
+        // General
+        [HarmonyPatch(typeof(MenuMain), "Start")]
+        public class Start
+        {
+            private static byte savedValue = FPSaveManager.badges[0];
+
+            static void Prefix()
+            {
+                FPSaveManager.badges[0] = 1;
+            }
+
+            static void Postfix()
+            {
+                FPSaveManager.badges[0] = savedValue;
+            }
+        }
+    }
+}
